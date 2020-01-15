c199fc34c3fe309036c02affdf8d225fca8d6f6c
tdnshah <sanketejas@gmail.com>
2020-01-16 00:16:03 +0530

    Contact form email added

---
+++ sendMail.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+$your_name = $_POST['name'];
+$your_email = $_POST['emailid'];
+$video_style = $_POST['syvideostyle'];
+$video_length = $_POST['hlyayvideo2b'];
+$form_message = $_POST['form_message'];
+
+$to = 'sankettejas@gmail.com';
+$subject = "Inquiry on creavids.co";
+$message = "Hi Creavids, You have got and enquiry on website from " . $your_name . " below are the contact details and message of the " . $your_name . ". EmailId " . $your_email . " Video Style " . $video_style . " Video Lenght " . $video_length . ". And the message he left is " . $form_message . " please contact as soon as possible.";
+
+$header = "From:admin@creavids.co \r\n";
+$header .= "MIME-Version: 1.0\r\n";
+$header .= "Content-type: text/html\r\n";
+
+print_r($message);
+
+$retval = mail($to, $subject, $message, $header);
+
+if ($retval == TRUE) {
+  echo "Message sent successfully...";
+}
+else {
+  echo "Message could not be sent...";
+}
