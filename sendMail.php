cd5f95f61c63781239900609accdba6f153d3f08
tdnshah <sanketejas@gmail.com>
2020-01-16 00:20:27 +0530

    Contact form email added

--- sendMail.php
+++ sendMail.php
@@ -10,7 +10,7 @@ $form_message = $_POST['form_message'];
 
 $to = 'sankettejas@gmail.com';
 $subject = "Inquiry on creavids.co";
-$message = "Hi Creavids, You have got and enquiry on website from " . $your_name . " below are the contact details and message of the " . $your_name . ". EmailId " . $your_email . " Video Style " . $video_style . " Video Lenght " . $video_length . ". And the message he left is " . $form_message . " please contact as soon as possible.";
+$message = "Hi Creavids, You have got and enquiry on website from <strong>" . $your_name . "</strong> below are the contact details and message of the " . $your_name . ". EmailId " . $your_email . " Video Style " . $video_style . " Video Lenght " . $video_length . ". And the message he left is " . $form_message . " please contact as soon as possible.";
 
 $header = "From:admin@creavids.co \r\n";
 $header .= "MIME-Version: 1.0\r\n";
