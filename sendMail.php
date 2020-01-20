e988826ae2aed15c4eed79354e42c16fb9e4a3a2
tdnshah <sanketejas@gmail.com>
2020-01-21 01:16:36 +0530

    Updated as per trello tasks

--- sendMail.php
+++ sendMail.php
@@ -12,7 +12,7 @@ $your_email = $_POST['emailid'];
 $video_style = $_POST['syvideostyle'];
 $video_length = $_POST['hlyayvideo2b'];
 $form_message = $_POST['form_message'];
-$to = 'sankettejas@gmail.com';
+$to = 'info@creavids.co';
 $subject = "Inquiry on creavids.co";
 $message = "Hi Creavids, You have got and enquiry on website from " . $your_name . " below are the contact details and message of the " . $your_name . ". EmailId " . $your_email . " Video Style " . $video_style . " Video Lenght " . $video_length . ". And the message he left is " . $form_message . " please contact as soon as possible.";
 
