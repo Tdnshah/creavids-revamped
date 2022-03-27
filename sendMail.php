632081f3de1b8ecbee00659e9f1ed832271151f6
tdnshah <sankettejas@gmail.com>
2022-03-27 17:25:59 +0530

    Update php require class paths.

--- sendMail.php
+++ sendMail.php
@@ -1,12 +1,14 @@
 <?php
 
 
+
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'phpmailer/Exception.php';
-require 'phpmailer/PHPMailer.php';
-require 'phpmailer/SMTP.php';
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
 
 $smtpUsername = 'info@creavids.co';
 $smtpPassword = 'creavids212#$'; 
