052a002fb563834ade9b6e48a14eadee8d0c91a6
tdnshah <sankettejas@gmail.com>
2022-03-27 18:07:07 +0530

    Update php require class paths.

--- sendMail.php
+++ sendMail.php
@@ -14,22 +14,23 @@ $smtpUsername = 'info@creavids.co';
 $smtpPassword = 'creavids212#$'; 
 
 
-$mail = new PHPMailer;
-$mail->isSMTP(); 
-$mail->SMTPDebug = 2; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
-$mail->Host = "smtp.gmail.com"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Mailer = "smtp";
+$mail->SMTPDebug = 1; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
+$mail->SMTPAuth = TRUE;
 $mail->Port = 587; // TLS only
-$mail->SMTPSecure = 'tls'; // ssl is depracated
-$mail->SMTPAuth = true;
+$mail->SMTPSecure = "tls"; // ssl is depracated
+$mail->Host = "smtp.gmail.com"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
 $mail->Username = $smtpUsername;
 $mail->Password = $smtpPassword;
-$mail->setFrom($emailFrom, $smtpUsername);
-$mail->addAddress($emailTo, 'sankettejas@gmail.com');
+$mail->setFrom($smtpUsername, 'Creavids Admin');
+$mail->addAddress("sankettejas@gmail.com", 'Tejas Shah');
 $mail->Subject = 'PHPMailer GMail SMTP test';
 $mail->msgHTML("test body"); //$mail->msgHTML(file_get_contents('contents.html'), __DIR__); //Read an HTML message body from an external file, convert referenced images to embedded,
 $mail->AltBody = 'HTML messaging not supported';
 // $mail->addAttachment('images/phpmailer_mini.png'); //Attach an image file
-
+$mail->send();
 if(!$mail->send()){
     print_r("Mailer Error: " . $mail->ErrorInfo;);
 }else{
