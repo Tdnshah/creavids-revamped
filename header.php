26a78a7e0696fe57915594b3e104f12ed99a53af
tdnshah <sankettejas@gmail.com>
2022-05-19 00:28:40 +0530

    Updated base with hostname.

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 include('env.php');
 
 $hostName = $_SERVER['HTTP_HOST'];
-echo $hostName;
 
 $currentPage = $_SERVER['REQUEST_URI'];
 $currentPage = str_replace('/', '', $currentPage);
@@ -26,7 +25,7 @@ function echoKeyword($keywords)
 <html lang="en">
 
 <head>
-  <!-- <base href="<?php echo getenv('CREAVIDS_SERVER_ENV') ?>"> -->
+  <base href="<?php echo $hostName ?>">
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
